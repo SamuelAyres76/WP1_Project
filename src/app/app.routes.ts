cd2142e80ac6fd8452ef232c8cb9847888f3e084
GreenThunder <123633616+SamuelAyres76@users.noreply.github.com>
2024-04-24 11:46:43 +0100

    Favourites page reachable (no content yet)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { Component } from '@angular/core';
+import { FavouritesComponent } from './favourites/favourites/favourites.component';
 
 export const routes: Routes = [
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'favourites', component: FavouritesComponent}
 ];
